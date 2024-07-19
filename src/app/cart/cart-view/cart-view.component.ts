beaa1121ec3866c4188540d2d56ff7cb9dd220c2
Aayush-Koju <iush.cozu12@gmail.com>
2024-07-19 21:40:04 +0545

    calculating total price and styling it

--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -10,13 +10,23 @@ import { Product } from 'src/app/models/product';
 export class CartViewComponent implements OnInit {
 
   cartItems : Product[] = [];
+  totalPrice: number = 0;
 
   constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
     this.cartService.getCartItems().subscribe(data =>{
       this.cartItems = data;
+      this.totalPrice = this.getTotalPrice();
     })
+  }
+
+  getTotalPrice(): number {
+    let total = 0;
+      for(let item of this.cartItems) {
+        total += item.price;
+      }
+      return total;
   }
 
 }
