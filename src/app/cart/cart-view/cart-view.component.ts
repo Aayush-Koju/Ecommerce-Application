56ce0a7893a331394f512816679d0764ec62f786
Aayush-Koju <iush.cozu12@gmail.com>
2024-07-20 21:46:20 +0545

    adding the checkout functionality

--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -33,4 +33,8 @@ export class CartViewComponent implements OnInit {
     this.cartService.clearCart().subscribe();
   }
 
+  checkout(): void {
+    this.cartService.checkOut(this.cartItems).subscribe();
+  }
+
 }
